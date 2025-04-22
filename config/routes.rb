3998bce8cf098e51a2c7216e855a47284076d01a
loganoppenheime <64404164+HUYQUANGTRAN2@users.noreply.github.com>
2025-04-22 06:10:29 +0000

    quang's 1st commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-
   # get "/your_first_screen" => "pages#first"
-  
+  get("/",{:controller =>"homepage",:action =>"displaying"})
+  get("dice/:number_of_dice/:how_many_sides",{:controller => "rolling", :action => "dynamic"}) 
 end
